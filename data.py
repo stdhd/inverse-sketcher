92ed3439d249777f8d86a4210aa53d9f937f080f
Armand Rousselot <armand.r@live.de>
2020-06-19 13:12:04 +0200

    added data loading pipeline

--- data.py
+++ data.py
@@ -1,22 +1,93 @@
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data.dataset import Subset
+from sklearn.model_selection import KFold
+import logging
+import os
 import torch
-from torch.utils.data import Dataset, DataLoader, TensorDataset
-import torchvision.transforms as T
-import torchvision.datasets
+from PIL import Image
+import torchvision
+
+logger = logging.getLogger(__name__)
 
 
-class SketchyVectors(torch.utils.data.Dataset):
+class ImageMetaData(object):
 
-    def __init__(self, split='train'):
-        super().__init__()
+    def __init__(self, path_sketch, path_real, label):
+        self.__label = label
+        self.__path_sketch = path_sketch
+        self.__path_real = path_real
+
+    def get_sketch(self):
+        return self.__path_sketch
+
+    def get_real(self):
+        return self.__path_real
+
+    def get_class(self):
+        return self.__label
 
 
-    def __getitem__(self, idx):
-        '''Implements the ``[idx]`` method. Here we convert the numpy data to
-        torch tensors.
-        '''
+class ImageDataSet(Dataset):
 
-        #return sketch_condition, photo
+    def __init__(self, root_dir, transform=None, return_path=False):
+        """
+        root_dir: directory of the dataset
+        include_unk: Whether to include the unknown class
+        transform: transormations to be applied every time a batch is loaded
+        """
+        self.__sketch_dir = os.path.join(root_dir,  "sketch")
+        self.__real_dir = os.path.join(root_dir,  "photo")
+        self.__transform = transform
+        self.__meta = list()
+        self.return_path = return_path
+
+        self.get_class_numbers()
+        self.__process_meta()
+
+
+    def get_class_numbers(self):
+        dict = {}
+        for i, classname in enumerate(os.listdir(self.__sketch_dir)):
+            dict[classname] = i
+        self.__class_dict = dict
+
+    def __process_meta(self):
+        #class1, class2, ...
+        for classname in os.listdir(self.__sketch_dir):
+            #label = self.class_numbers.get(classname)
+            #if label is None:
+            #    logger.error("Warning: Undefined class name {} in data directory {}".format(classname, self.__root_dir))
+            for filename in os.listdir(os.path.join(self.__sketch_dir, classname)):
+                path_sketch = os.path.join(self.__sketch_dir, classname, filename)
+                path_real = os.path.join(self.__real_dir, classname, filename.split("-")[0] + ".jpg")
+                if not os.path.exists(path_real):
+                    logger.error("Warning: Could not find real image named {} corresponding to sketch {}".format(path_real, path_sketch))
+                    continue
+                #if not self.load_on_request:
+                #    image = torch.from_numpy(cv2.imread(path))
+                self.__meta.append(ImageMetaData(path_sketch, path_real, self.__class_dict[classname]))
+        print("Processed {} sketches".format(len(self.__meta)))
+
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.__meta)
 
+    def __getitem__(self, idx):
+        if type(idx) == torch.Tensor:
+            idx = idx.to(dtype=torch.int)
+        meta = self.__meta[idx]
+
+        path_sketch = meta.get_sketch()
+        path_real = meta.get_real()
+
+        sketch = Image.open(path_sketch).convert("L")
+        image = Image.open(path_real)
+
+        if self.__transform is not None:
+            sketch = self.__transform(sketch)
+            image = self.__transform(image)
+
+        tensor_transform = torchvision.transforms.ToTensor()
+        image = tensor_transform(image)
+        sketch = tensor_transform(sketch)
+        return sketch, image, meta.get_class()
--- train.py
+++ train.py
@@ -6,9 +6,13 @@ import torch.nn
 import torch.optim
 import numpy as np
 
-import model
+#import model
 import data
 import argparse
+import math
+from torch.utils.data import DataLoader
+import os
+import torchvision
 
 # Determine, whether cuda will be enabled
 # use x.to_device(args.device)
@@ -26,32 +30,46 @@ else:
 
 # Define dictionary of hyper parameters
 list_hyper_params = [
-    {"batch_size": 100, "n_epochs": 5, "learning_rate": 0.01},
-    {"batch_size": 100, "n_epochs": 5, "learning_rate": 0.001}
+    {"batch_size": 16, "n_epochs": 5, "learning_rate": 0.01, "test_ratio":0.1},
+    {"batch_size": 16, "n_epochs": 5, "learning_rate": 0.001, "test_ratio":0.1}
 ]
 
-# Loop over hyper parameter configurations
-for params in list_hyper_params:
-    # initialize model with custom parameters
-    #m = model.CINN(params)
-    #scheduler = torch.optim.lr_scheduler.MultiStepLR(cinn.optimizer, milestones=[20, 40], gamma=0.1)
-    t_start = time()
+def create_dataloaders(data_path, batch_size, test_ratio, split=None):
+    data_set = data.ImageDataSet(root_dir=data_path)
+    if split is None:
+        train_split, test_split = torch.utils.data.random_split(data_set, [math.ceil(len(data_set)*(1-test_ratio)), math.floor(len(data_set)*(test_ratio))])
 
-    for epoch in range(params["n_epochs"]):
-        print("Start training for " + str(params))
-        for batch, (x, l) in enumerate(data.train_loader):
-            pass # TODO training
-        #scheduler.step()
+    dataloader_train = DataLoader(train_split, batch_size=batch_size, shuffle=True, num_workers=os.cpu_count(), drop_last=True)
+    dataloader_test = DataLoader(test_split, batch_size=batch_size, shuffle=False, num_workers=os.cpu_count())
 
-    #### End of training round single hyper parameter setting
-    filename = str(params)\
-        .replace('{', '')\
-        .replace('}', '')\
-        .replace("'", '')\
-        .replace(' ', '')\
-        .replace(':', '')\
-        .replace(',', '_')
+    return dataloader_train, dataloader_test
 
-    # Save state to file
-    #torch.save(m.state_dict(), 'output/' + filename + '.pt')
-    print('%.3i \t%.6f' % (epoch, (time() - t_start) / 60.))
+if __name__ == "__main__":
+    # Loop over hyper parameter configurations
+    for params in list_hyper_params:
+        # initialize model with custom parameters
+        #m = model.CINN(params)
+        #scheduler = torch.optim.lr_scheduler.MultiStepLR(cinn.optimizer, milestones=[20, 40], gamma=0.1)
+        dataloader_train, dataloader_test = create_dataloaders("dataset/SketchyDatabase/256x256", params["batch_size"], params["test_ratio"])
+
+        t_start = time()
+
+        for epoch in range(params["n_epochs"]):
+            print("Start training for " + str(params))
+            for batch, (sketch, real, label) in enumerate(dataloader_train):
+                print(sketch.shape, real.shape, label)
+                exit()
+            #scheduler.step()
+
+        #### End of training round single hyper parameter setting
+        filename = str(params)\
+            .replace('{', '')\
+            .replace('}', '')\
+            .replace("'", '')\
+            .replace(' ', '')\
+            .replace(':', '')\
+            .replace(',', '_')
+
+        # Save state to file
+        #torch.save(m.state_dict(), 'output/' + filename + '.pt')
+        print('%.3i \t%.6f' % (epoch, (time() - t_start) / 60.))
