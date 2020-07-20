64fa67de3d9cb1a5b62c943b5b19e1dce0a854bd
Stephan <44555785+stdhd@users.noreply.github.com>
2020-07-20 22:26:16 +0200

    print dataset labels

--- metrics/inception_score.py
+++ metrics/inception_score.py
@@ -46,7 +46,8 @@ def inception_score(imgs, cuda=True, batch_size=32, resize=False, splits=1):
     # Get predictions
     preds = np.zeros((N, 1000))
 
-    for i, batch in enumerate(dataloader, 0):
+    for i, (batch, labl) in enumerate(dataloader, 0):
+        print(labl)
         batch = batch.type(dtype)
         batchv = Variable(batch)
         batch_size_i = batch.size()[0]
